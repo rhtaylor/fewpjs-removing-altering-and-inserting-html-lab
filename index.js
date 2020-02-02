b6467c5c74f518d7c04facc10dd685b69abd81d1
Randy <randyisbusy@gmail.com>
2020-02-01 17:56:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here! 
+
+let remove = document.querySelector('main')  
+remove.remove() 
+
+let newHeader = document.createElement('h1') 
+
+newHeader.setAttribute("id", "victory")
+console.log(newHeader)
+newHeader.innerHTML = "is the champion"
